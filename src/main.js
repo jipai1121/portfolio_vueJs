6ab243cde69443ba80ffd6da9f41b72851978e25
LAPTOP-KM9VP3E2\jipai <jipai1121@gmail.com>
2023-02-26 13:47:32 +0900

    add: main.js追加

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { i18n } from './i18n.js'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+Vue.config.productionTip = false
+
+new Vue({
+  i18n: i18n,
+  render: h => h(App),
+}).$mount('#app')
